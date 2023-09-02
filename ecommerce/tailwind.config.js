179fdb09e71c42772d94f998036fcc687dda07ed
Muhammad Hannan <mdhannan.info@gmail.com>
2023-09-03 00:55:03 +0600

    Added header section

--- ecommerce/tailwind.config.js
+++ ecommerce/tailwind.config.js
@@ -1,9 +1,23 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
+    screens:{
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container:{
+      center: true,
+      padding: "1rem",
+    },
     extend: {
+      fontFamily:{
+        roboto: "'Roboto', sans-serif",
+        poppins: "'Poppins', sans-serif"
+      },
       colors: {
-        'daily-dev-tips': "#F89283"
+        'primary': "#FD3057"
       }
     },
   },
